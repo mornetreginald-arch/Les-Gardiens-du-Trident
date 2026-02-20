bdec7c88289b247625851215491460bdaa077682
Réginald <mornet.reginald@gmail.com>
2026-02-20 10:38:34 +0100

    modification du formulaire en frontend

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,31 +2,58 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
+public function login(
+    Request $request,
+    AuthenticationUtils $authenticationUtils,
+    UserPasswordHasherInterface $passwordHasher,
+    EntityManagerInterface $entityManager
+): Response {
 
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
+    $error = $authenticationUtils->getLastAuthenticationError();
+    $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ]);
+    $registrationForm = $this->createForm(RegistrationFormType::class);
+    $registrationForm->handleRequest($request);
+
+    $registerError = false;
+
+    if ($registrationForm->isSubmitted()) {
+        $registerError = true;
+
+        if ($registrationForm->isValid()) {
+            $user = $registrationForm->getData();
+
+            $user->setPassword(
+                $passwordHasher->hashPassword(
+                    $user,
+                    $registrationForm->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
-    {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-    }
+    return $this->render('security/login.html.twig', [
+        'registrationForm' => $registrationForm->createView(),
+        'last_username' => $lastUsername,
+        'error' => $error,
+        'registerError' => $registerError,
+    ]);
+}
 }
