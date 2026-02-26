340bd294617ea3703a9ca50a599fa348d3c174a4
Réginald <mornet.reginald@gmail.com>
2026-02-26 17:40:58 +0100

    commencement des modals panier

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,10 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -14,30 +12,42 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
-    {
-        $user = new User();
-        $form = $this->createForm(RegistrationFormType::class, $user);
-        $form->handleRequest($request);
+    #[Route('/register', name: 'app_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager
+    ): Response {
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $plainPassword */
-            $plainPassword = $form->get('plainPassword')->getData();
+        $data = $request->request->all('registration_form');
 
-            // encode the plain password
-            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
-            return $security->login($user, 'form_login', 'main');
+        // Sécurité minimale
+        if (!$data) {
+            return $this->redirectToRoute('app_articles_index');
         }
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form,
-        ]);
+        $user = new User();
+
+        $user->setEmail($data['email'] ?? null);
+        $user->setNom($data['nom'] ?? null);
+        $user->setPrenom($data['prenom'] ?? null);
+        $user->setTelephone($data['telephone'] ?? null);
+        $user->setRue($data['rue'] ?? null);
+        $user->setCodePostal($data['code_postal'] ?? null);
+        $user->setVille($data['ville'] ?? null);
+        $user->setPays($data['pays'] ?? null);
+
+        // Hash du mot de passe
+        $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                $user,
+                $data['plainPassword'] ?? ''
+            )
+        );
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_articles_index');
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,64 +2,36 @@
 
 namespace App\Controller;
 
-use App\Form\RegistrationFormType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route as AttributeRoute;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-public function login(
-    Request $request,
-    AuthenticationUtils $authenticationUtils,
-    UserPasswordHasherInterface $passwordHasher,
-    EntityManagerInterface $entityManager
-): Response {
-
-    $error = $authenticationUtils->getLastAuthenticationError();
-    $lastUsername = $authenticationUtils->getLastUsername();
-
-    $registrationForm = $this->createForm(RegistrationFormType::class);
-    $registrationForm->handleRequest($request);
-
-    $registerError = false;
-
-    if ($registrationForm->isSubmitted()) {
-        $registerError = true;
-
-        if ($registrationForm->isValid()) {
-            $user = $registrationForm->getData();
-
-            $user->setPassword(
-                $passwordHasher->hashPassword(
-                    $user,
-                    $registrationForm->get('plainPassword')->getData()
-                )
-            );
-
-            $entityManager->persist($user);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_login');
+    #[AttributeRoute(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // Si l'utilisateur est déjà connecté
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
         }
+
+        // Récupération des erreurs éventuelles
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
-    return $this->render('security/login.html.twig', [
-        'registrationForm' => $registrationForm->createView(),
-        'last_username' => $lastUsername,
-        'error' => $error,
-        'registerError' => $registerError,
-    ]);
-}
-
-#[Route(path: '/logout', name: 'app_logout')]
-public function logout(): void
-{
-    // throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
-}
+    #[AttributeRoute(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Symfony intercepte automatiquement cette route
+        throw new \LogicException('Cette méthode peut rester vide.');
+    }
 }
