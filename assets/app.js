3de824235f24082b9f5fcca959ee962a93020925
Réginald <mornet.reginald@gmail.com>
2026-04-16 15:30:16 +0200

    rectification du logo

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,8 @@ import './stimulus_bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
